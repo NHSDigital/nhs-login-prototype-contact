01ab55822b45ebfc3f38a8c936097b7c027808d2
Alex Colbeck <alexcolbeck@gmail.com>
2020-02-14 10:26:09 +0000

    Refined scope v9

--- app/routes.js
+++ app/routes.js
@@ -20,18 +20,83 @@ router.post('/examples/branching/answer', function (req, res) {
   }
 });
 
-router.post('/account/v7/other/other-add-next', function (req, res) {
 
+// Route the user to the correct data entry page depending on whether they select 'phone' or 'email'
+router.post('/account/v7/other/other-add-next', function (req, res) {
   var patientEmail = req.session.data['patient-email']
   var patientMobile = req.session.data['patient-mobile']
-
   if (patientEmail == "" && patientMobile == ""){
     res.redirect('/account/v7/other/other-type')
   }
   else {
     res.redirect('/account/v7/index')
   }
+})
 
+router.post('/account/v9/more/more-add-3', function (req, res) {
+  var patientEmail = req.session.data['patient-email']
+  var patientMobile = req.session.data['patient-mobile']
+  if (patientEmail == "" && patientMobile == ""){
+    res.redirect('/account/v9/more/more-type')
+  }
+  else {
+    res.redirect('/account/v9/index')
+  }
+})
+
+router.post('/account/v9/more/more-type-', function (req, res) {
+  var type = req.session.data['more-type']
+  if (type == "address"){
+    res.redirect('/account/v9/address/address-add')
+  }
+  else if (type == "phone"){
+    res.redirect('/account/v9/phone/phone-add')
+  }
+  else if (type == "email"){
+    res.redirect('/account/v9/email/email-add')
+  }
+  else {
+  }
+})
+
+router.post('/account/v9/phone/patient-change-login-mobile', function (req, res) {
+  var choice = req.session.data['patient-change-login-mobile']
+  if (choice == "yes"){
+    res.redirect('/account/v9/manage-login?patient-mobile=')
+  }
+  else if (choice == "no"){
+    res.redirect('/account/v9/index?patient-mobile=')
+  }
+})
+
+router.post('/account/v9/email/patient-change-login-email', function (req, res) {
+  var choice = req.session.data['patient-change-login-email']
+  if (choice == "yes"){
+    res.redirect('/account/v9/manage-login?patient-email=')
+  }
+  else if (choice == "no"){
+    res.redirect('/account/v9/index?patient-email=')
+  }
+})
+
+router.post('/account/v9/email/email-add-to-contact', function (req, res) {
+  var choice = req.session.data['email-add-to-contact']
+  if (choice == "yes"){
+    res.redirect('/account/v9/email/email-change-done-done')
+  }
+  else if (choice == "no"){
+    res.redirect('/account/v9/manage-login')
+  }
+})
+
+router.post('/account/v9/phone/phone-add-to-contact', function (req, res) {
+  var choice = req.session.data['phone-add-to-contact']
+  if (choice == "yes"){
+    res.redirect('/account/v9/phone/phone-change-done-done')
+  }
+  else if (choice == "no"){
+    res.redirect('/account/v9/manage-login')
+  }
 })
 
 
